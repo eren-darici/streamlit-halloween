933558caeeb6bebd082cbbdfa2e5b53150ca30c8
Eren DARICI <ed.erendarici@gmail.com>
2023-12-26 19:39:19 -0500

    v1?

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import streamlit as st
-import requests
+from openai_client import MyOpenAIClient
+import json
 
 # Streamlit app layout
 st.title("Halloween Costume Generator")
@@ -8,18 +9,17 @@ st.title("Halloween Costume Generator")
 st.sidebar.header("Select costume specs")
 
 # Select medium(s)
-medium_list = ["TV", "Comics", "Movies"]
-medium = st.sidebar.multiselect("Select your favorite medium:", medium_list)
+medium_list = ["TV", "Comics", "Movies", "Music", "Anime", "Manga"]
+medium = st.sidebar.multiselect("Select your favorite medium:", medium_list, help="Select at least one medium.")
 
 # Budget selectbox with labels
 budget_levels = ["Low", "Medium", "High"]
-budget = st.sidebar.selectbox("Select your budget:", budget_levels)
-
+budget = st.sidebar.selectbox("Select your budget:", budget_levels, help="Select a budget level.")
 
 # Add personal info
 ## Gender multiselect
 gender_options = ["Male", "Female", "Non-Binary"]
-selected_gender = st.sidebar.multiselect("Select your gender:", gender_options)
+selected_gender = st.sidebar.multiselect("Select your gender:", gender_options, help="Select at least one gender.")
 
 ## Height
 ### Height selectbox
@@ -27,29 +27,62 @@ st.sidebar.subheader("Select your height:")
 
 ### Selectbox for feet
 height_feet_options = list(range(4, 8))
-selected_height_feet = st.sidebar.selectbox("Feet:", height_feet_options)
+selected_height_feet = st.sidebar.selectbox("Feet:", height_feet_options, help="Select your height in feet.")
 
 ### Selectbox for inches
 height_inches_options = list(range(0, 12))
-selected_height_inches = st.sidebar.selectbox("Inches:", height_inches_options)
+selected_height_inches = st.sidebar.selectbox("Inches:", height_inches_options, help="Select your height in inches.")
 
 ### Combine feet and inches
 selected_height = f"{selected_height_feet}'{selected_height_inches}''"
 
 ## Weight
-weight = st.sidebar.number_input("Enter your weight (lbs):", min_value=0, value=150, step=1, max_value=500)
+weight = st.sidebar.number_input("Enter your weight (lbs):", min_value=0, value=150, step=1, max_value=500, help="Enter your weight in pounds.")
 
 ## Extras
 st.sidebar.header("Extras")
 
 ### Glasses yes/no
-glasses = st.sidebar.selectbox("Do you wear glasses?", ("Yes", "No"))
+glasses = st.sidebar.selectbox("Do you wear glasses?", ("Yes", "No"), help="Select whether you wear glasses or not.")
 
-params = {
-    "medium": medium,
-    "budget": budget,
-    "gender": selected_gender,
-    "height": selected_height,
-    "weight": weight,
-    "glasses": glasses,
-}
+# Button to generate costume
+if st.sidebar.button("Generate Costume"):
+    # Validate that mandatory fields are selected
+    if not medium or not budget or not selected_gender:
+        st.sidebar.warning("Please fill in all mandatory fields.")
+    else:
+        # Define parameters
+        params = {
+            "medium": medium,
+            "budget": budget,
+            "gender": selected_gender,
+            "height": selected_height,
+            "weight": weight,
+            "glasses": glasses,
+        }
+
+        # Call the GPT API and display results
+        client = MyOpenAIClient()
+        ideas = client.generate_costume_idea(params=params).strip('"').strip()
+        print(ideas)
+        
+        try:
+            # Try to parse ideas as JSON
+            ideas_json = json.loads(ideas)
+            print(ideas_json)
+
+            # Display ideas on the main screen in a card layout
+            st.subheader("Generated Costume Ideas:")
+            for idea in ideas_json:
+                st.write(f"## {idea['costume']}")
+
+                # Display the costume image
+                image_url = client.generate_costume_image(prompt=idea['dalle_prompt'])
+                st.image(image_url, caption="Costume Image", use_column_width=True)
+
+
+                for prop in idea['props']:
+                    st.write(f"   - {prop}")
+                st.markdown("---")  # Add a horizontal line between costume ideas for separation
+        except json.JSONDecodeError:
+            st.warning("Unable to parse response from the GPT API. Please try again or check the API response format.")
--- app/openai_client.py
+++ app/openai_client.py
@@ -15,10 +15,9 @@ class MyOpenAIClient():
 
         # Create client
         self.client = openai.OpenAI(api_key=self.OPENAI_API_KEY)
-
         
 
-    def generate_costum_idea(self, params):
+    def generate_costume_idea(self, params):
         # Define user prompt
         self.user_prompt = f"""
         You are my personal assistant to pick my halloween costume for this upcoming halloween.
@@ -26,6 +25,27 @@ class MyOpenAIClient():
         You only need to give me the name of the costume, props for that costume as a list.
         I'm a {params['height']} and {params['weight']}lbs {params['gender']}. I like {params['medium']}. My budget for this costume is {params['budget']}.
         You need to give me the costume from {params['medium']}, and it should be well known costume. Since it would be a cosplay, there is no copyright barrier.
+        Give me the ideas as following JSON format and MAKE SURE THAT CHARACTER LOOKS LIKE THE CHARACTER ITSELF AND NO OTHER CHARACTER (I will tip you 200$ more if you can do that):
+        in a list, for each costume:
+            costume: costume name (string)
+            props: list of props
+            dalle_prompt: a prompt for Dall-e, that contains both character and my specifications and include props.
+
+        example output:
+        "[
+            {{
+                'costume': 'costumename',
+                'props': ['a list of props'],
+                'dalle_prompt': 'prompt for dalle'
+            }},
+            {{
+                'costume': 'costumename',
+                'props': ['a list of props'],
+                'dalle_prompt': 'prompt for dalle'
+            }}
+        ]"
+
+        IMPORTANT! For the response, delete any trailing character besides JSON. IT IS IMPORTANT!
         """
 
         # Check extras
@@ -33,7 +53,7 @@ class MyOpenAIClient():
             self.user_prompt += f"I also wear glasses."
 
         # Define system prompt
-        self.system_prompt = "Sure! Here are some costume ideas from the mediums that you like:"
+        self.system_prompt = "Sure! Here are 3 costume ideas from the mediums that you like that are related to your specifications regarding your look and your budget:"
 
         # Define conversation
         conversation = [
@@ -43,13 +63,28 @@ class MyOpenAIClient():
 
         # Create response
         response = self.client.chat.completions.create(
-            model="gpt-3.5",
+            model="gpt-3.5-turbo-1106",
             messages=conversation
         )
 
-        # Strip message
+        # Get message
         message = response.choices[0].message.content
 
         return message
 
+    def generate_costume_image(self, prompt):
+        # Create response
+        response = self.client.images.generate(
+            model="dall-e-3",
+            prompt=prompt,
+            size="1024x1024",
+            quality="standard",
+            n=1,
+        )
+
+        # Get Image URL
+        image_url = response.data[0].url
+        print(prompt)
+
+        return image_url
         
