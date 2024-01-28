6716987d7748f001f398c8710e9dd85dc95feb7e
Eren DARICI <ed.erendarici@gmail.com>
2024-01-28 15:52:20 -0500

    Create pydantic model

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel, Field
+from typing import List, Optional
+
+
+class Costume(BaseModel):
+    name: str
+    medium: str
+    gender: List[str]
+    budget: str
+    height: float
+    glasses: Optional[str] = Field(None, description="Select whether you wear glasses or not.")
+    hair: Optional[str] = Field(None, description="Select your hair length.")
+    weight: float
+    age: Optional[int] = Field(None, description="Enter your age.")
