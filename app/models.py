b36881a25e7c71fbc919b3105b91391ac2d4ae2d
Eren DARICI <ed.erendarici@gmail.com>
2024-01-28 15:54:17 -0500

    Update pydantic model

--- app/models.py
+++ app/models.py
@@ -10,6 +10,7 @@ class Costume(BaseModel):
     height: float
     glasses: Optional[str] = Field(None, description="Select whether you wear glasses or not.")
     hair: Optional[str] = Field(None, description="Select your hair length.")
+    ethnicity: Optional[str]
     weight: float
     age: Optional[int] = Field(None, description="Enter your age.")
 
